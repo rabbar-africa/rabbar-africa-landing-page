c09e0db0a54ef8b53ae2419eec948db140de820a
ayodeji <afuwapeayodeji2018@gmail.com>
2025-11-13 20:25:46 +0100

    feat: fixed build issues

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  hostname: "rabbar.africa",
   images: {
-    domains: ["cdn.sanity.io", "images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
